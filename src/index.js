1abe9051f713458a397b805164b08f7500c5717d
Dipanjan Jayswal <iamdipanjanjayswal@gmail.com>
2021-05-20 14:27:57 +0530

    used the props.children to show data from a child

--- src/index.js
+++ src/index.js
@@ -1,27 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">Sam</a>
-                    <div className="metadata">
-                        <span className="date">Today at 6.00PM</span>
-                    </div>
-                    <div className="text">Nice Work</div>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4.45PM"
+          userComment="Nice work"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Dip"
+          timeAgo="Today at 5.00PM"
+          userComment="Nice work buddy"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jay"
+          timeAgo="Today at 2.15PM"
+          userComment="Appreciatable"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Tim"
+          timeAgo="Today at 11.15AM"
+          userComment="Awesome work"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render(
-    <App />,
-    document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
