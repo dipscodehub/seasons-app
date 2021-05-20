d8a0a08497a282206f5d74d343e605c47e2b9bae
Dipanjan Jayswal <iamdipanjanjayswal@gmail.com>
2021-05-20 15:00:46 +0530

    seasons app using class based components

--- src/index.js
+++ src/index.js
@@ -1,45 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
 
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
-
+import SeasonDisplay from "./SeasonDisplay";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail
-          author="Sam"
-          timeAgo="Today at 4.45PM"
-          userComment="Nice work"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Dip"
-          timeAgo="Today at 5.00PM"
-          userComment="Nice work buddy"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Jay"
-          timeAgo="Today at 2.15PM"
-          userComment="Appreciatable"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Tim"
-          timeAgo="Today at 11.15AM"
-          userComment="Awesome work"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
+     <SeasonDisplay />
     </div>
   );
 };
